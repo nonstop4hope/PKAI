0aa81e3fdd99fa4f0b22f5cd9cba79a31b40d4ca
nonstop4hope <igormitusov@mail.ru>
2022-07-11 14:31:18 +0000

    add postgresql connection

--- PKAI/settings.py
+++ PKAI/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'PKAI.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.getenv('DATABASE_NAME'),
+        'USER': os.getenv('POSTGRESQL_USER'),
+        'PASSWORD': os.getenv('POSTGRESQL_PASSWORD'),
+        'HOST': os.getenv('POSTGRESQL_HOST'),
+        'PORT': os.getenv('POSTGRESQL_PORT'),
     }
 }
 
