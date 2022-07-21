47ef89a0fdb14dd46fa009e518250604e5334dd3
mrSakur <mrsakur@mail.ru>
2022-07-22 00:45:28 +0300

    Init core

--- apps/search/urls.py
+++ apps/search/urls.py
@@ -4,5 +4,6 @@ from apps.search import views
 
 urlpatterns = [
     path('zenodo', views.get_zenodo_records, name='zenodo_search'),
+    path('core', views.get_core_records, name='core_search'),
     path('result', views.get_celery_result_by_id),
 ]
--- apps/search/views.py
+++ apps/search/views.py
@@ -4,6 +4,7 @@ from django.http import JsonResponse
 
 from .celery_result import get_task_state_by_id
 from .tasks import get_zenodo_records_async
+from .tasks import get_core_records_async
 
 
 def get_zenodo_records(request):
@@ -13,6 +14,16 @@ def get_zenodo_records(request):
         return JsonResponse({'task_id': str(get_zenodo_records_async.delay(search_query, page))})
     else:
         return JsonResponse({"detail": "Authentication credentials were not provided."}, status=HTTPStatus.FORBIDDEN)
+
+
+def get_core_records(request):
+    # if request.user.is_authenticated:
+    search_query = request.GET.get('query')
+    page = request.GET.get('page')
+    core_records = str(get_core_records_async.delay(search_query, page))
+    return JsonResponse({'task_id': core_records})
+    # else:
+    #     return JsonResponse({"detail": "Authentication credentials were not provided."}, status=HTTPStatus.FORBIDDEN)
 
 
 def get_celery_result_by_id(request):
