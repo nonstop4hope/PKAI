95c2e124f82f097b35304071cbe5834e7336c472
mrSakur <mrsakur@mail.ru>
2022-07-25 12:45:24 +0300

    remove authenticated for core

--- apps/search/views.py
+++ apps/search/views.py
@@ -19,7 +19,7 @@ def get_zenodo_records(request):
 def get_core_records(request):
     # if request.user.is_authenticated:
     search_query = request.GET.get('query')
-    page = request.GET.get('page')
+    page = request.GET.get('page', 1)
     core_records = str(get_core_records_async.delay(search_query, page))
     return JsonResponse({'task_id': core_records})
     # else:
@@ -27,9 +27,9 @@ def get_core_records(request):
 
 
 def get_celery_result_by_id(request):
-    if request.user.is_authenticated:
-        task_id = request.GET.get('task_id')
-        response = get_task_state_by_id(task_id)
-        return JsonResponse(response)
-    else:
-        return JsonResponse({"detail": "Authentication credentials were not provided."}, status=HTTPStatus.FORBIDDEN)
+    # if request.user.is_authenticated:
+    task_id = request.GET.get('task_id')
+    response = get_task_state_by_id(task_id)
+    return JsonResponse(response)
+    # else:
+    #     return JsonResponse({"detail": "Authentication credentials were not provided."}, status=HTTPStatus.FORBIDDEN)
