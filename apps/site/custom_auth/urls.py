1f4e7b03fffc4347605a27e852acede3c1a20e1e
mrSakur <mrsakur@mail.ru>
2022-07-25 23:29:42 +0300

    Fix

--- apps/site/custom_auth/urls.py
+++ apps/site/custom_auth/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
-from .views import MyObtainTokenPairView, RegisterView, test
+from .views import MyObtainTokenPairView, RegisterView, login, user_logout
 from rest_framework_simplejwt.views import TokenRefreshView
 
 
 urlpatterns = [
-    path('test/', test, name='test'),
-    path('login/', MyObtainTokenPairView.as_view(), name='token_obtain_pair'),
+    path('login/', login, name='login'),
+    path('logout/', user_logout, name='login'),
+    # path('login/', MyObtainTokenPairView.as_view(), name='token_obtain_pair'),
     path('login/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('register/', RegisterView.as_view(), name='auth_register'),
 ]
--- apps/site/custom_auth/views.py
+++ apps/site/custom_auth/views.py
@@ -1,23 +1,12 @@
+from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.models import User
+from django.http import HttpResponse
+from django.shortcuts import redirect
 from rest_framework import generics
 from rest_framework.permissions import AllowAny
 from rest_framework_simplejwt.views import TokenObtainPairView
 
 from .serializers import MyTokenObtainPairSerializer, RegisterSerializer
-from django.contrib.auth import authenticate, login
-from django.shortcuts import redirect
-from django.http import HttpResponse
-
-
-def test(request):
-    username = request.POST['username']
-    password = request.POST['password']
-    user = authenticate(request, username=username, password=password)
-    if user is not None:
-        login(request, user)
-        return redirect('/')
-    else:
-        return HttpResponse('Unauthorized', status=401)
 
 
 class MyObtainTokenPairView(TokenObtainPairView):
@@ -29,3 +18,27 @@ class RegisterView(generics.CreateAPIView):
     queryset = User.objects.all()
     permission_classes = (AllowAny,)
     serializer_class = RegisterSerializer
+
+
+def login(request):
+
+    user = None
+    username = request.POST.get('username')
+    password = request.POST.get('password')
+
+    if username and password:
+        user = authenticate(request, username=username, password=password)
+
+    if user is not None:
+        login(request, user)
+        return redirect('/')
+    else:
+        return HttpResponse('Unauthorized', status=401)
+
+
+def user_logout(request):
+
+    if request.user.is_authenticated:
+        logout(request)
+
+    return redirect('/')
