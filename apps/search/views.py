a56ac5c3c82559ad37eeb451f31230f66d3d12b4
mrSakur <mrsakur@mail.ru>
2022-07-26 12:40:25 +0300

    set authenticated for core

--- apps/search/views.py
+++ apps/search/views.py
@@ -17,13 +17,13 @@ def get_zenodo_records(request):
 
 
 def get_core_records(request):
-    # if request.user.is_authenticated:
-    search_query = request.GET.get('query')
-    page = int(request.GET.get('page', 1))
-    core_records = str(get_core_records_async.delay(search_query, page))
-    return JsonResponse({'task_id': core_records})
-    # else:
-    #     return JsonResponse({"detail": "Authentication credentials were not provided."}, status=HTTPStatus.FORBIDDEN)
+    if request.user.is_authenticated:
+        search_query = request.GET.get('query')
+        page = int(request.GET.get('page', 1))
+        core_records = str(get_core_records_async.delay(search_query, page))
+        return JsonResponse({'task_id': core_records})
+    else:
+        return JsonResponse({"detail": "Authentication credentials were not provided."}, status=HTTPStatus.FORBIDDEN)
 
 
 def get_celery_result_by_id(request):
