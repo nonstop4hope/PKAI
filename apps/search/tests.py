ce579758155ece65a0272c7e8ad319f5fe15f991
mrSakur <mrsakur@mail.ru>
2022-08-02 00:14:18 +0300

    add zenodo test

--- apps/search/tests.py
+++ apps/search/tests.py
@@ -1,7 +1,7 @@
 from rest_framework.test import force_authenticate
 from rest_framework.test import APIRequestFactory
 from django.contrib.auth.models import User
-from .views import get_celery_result_by_id, get_core_records
+from .views import get_celery_result_by_id, get_core_records, get_zenodo_records
 import json
 from django.test import TestCase
 
@@ -14,6 +14,34 @@ class CoreTest(TestCase):
     request.user = user
     force_authenticate(request, user=user)
     response = get_core_records(request)
+    print(response.content)
+    content = json.loads(response.content)
+    task_id = content.get('task_id')
+
+    task_status = "PENDING"
+
+    while True:
+        request = factory.get(f'/api/v1/search/result?task_id={task_id}')
+        request.user = user
+        force_authenticate(request, user=user)
+        response = get_celery_result_by_id(request)
+        content = json.loads(response.content)
+        task_status = content.get("task_status")
+
+        if task_status != "PENDING":
+            break
+
+    print(response.content)
+
+
+class ZenodoTest(TestCase):
+    factory = APIRequestFactory()
+    user = User.objects.get(username='test')
+
+    request = factory.get('/api/v1/search/zenodo?query=test&page=5586')
+    request.user = user
+    force_authenticate(request, user=user)
+    response = get_zenodo_records(request)
     print(response.content)
     content = json.loads(response.content)
     task_id = content.get('task_id')
